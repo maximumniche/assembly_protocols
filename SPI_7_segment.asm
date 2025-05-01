17bce3f422fcf0a983a154a651e1d5b294a07b21
Imad <imadaman99@gmail.com>
2025-05-01 19:47:53 -0400

    fixed code for "SPI.asm", now renamed to "SPI_7_segment"

--- SPI_7_segment.asm
+++ SPI_7_segment.asm
@@ -1,4 +1,4 @@
-; Program for outline of code to do MOSI SPI
+; Program for outline of code to do MOSI SPI with 7-segment MAX7219
 
 .cpu 6502
 .equ outputKIM, 0x1700 ; variable for address of SPI pins (where output will be sent from), bit 7 is MOSI, bit 6 is SS, bit 5 is SCLK
@@ -17,55 +17,80 @@ main:
     ; ideally you would put the setup and sending of address and data bytes in their own subroutines (which are like functions) to optimize process
 
     JSR setup
-
-    ; setup, need to send data to set decode mode, intensity, scan limit, and shutdown mode of MAX7219
-    set_up:
-
-        ; send data to set decode mode to code B decode for all digits
-        LDA #0x09
-        STA output
-        JSR byte_send
-        LDA #0xFF
-        STA output
-        JSR byte_send
-
-        ; send data to set intensity to max
-        LDA #0x0A
-        STA output
-        JSR byte_send
-        LDA #0x0F
-        STA output
-
-        ; send data to set scan limit to all digits
-        LDA #0x0B
-        STA output
-        JSR byte_send
-        LDA #0x07
-        STA output
-        JSR byte_send
-
-        ; send data to set shutdown mode to OFF aka normal operation
-        LDA #0x0C
-        STA output
-        JSR byte_send
-        LDA #0x01
-        STA output
-        JSR byte_send
+    JSR initialize
 
     ; send data for number 1 to digit 0 of display in code B decode
+    
+    ; send bytes to display 0 on digit 7 (Address 0x08 -> data[0])
+    LDA #0x08
+    STA byte1
+    LDA #0x0F
+    STA byte2
+    
+    JSR bytes_send
+    
+    ; display 0 on digit 6 (Address 0x07 -> data[1])
+    LDA #0x07
+    STA byte1
+    LDA #0x0F
+    STA byte2
+    
+    JSR bytes_send
+    
+    ; display 0 on digit 5 (Address 0x06 -> data[2])
+    LDA #0x06
+    STA byte1
+    LDA #0x0F
+    STA byte2
+    
+    JSR bytes_send
+    
+    ; display 0 on digit 4 (Address 0x05 -> data[3])
+    LDA #0x05
+    STA byte1
+    LDA #0x0F
+    STA byte2
+    
+    JSR bytes_send
+    
+    ; display 0 on digit 3 (Address 0x04 -> data[4])
+    LDA #0x04
+    STA byte1
+    LDA #0x0F
+    STA byte2
+    
+    JSR bytes_send
+    
+    ; display 0 on digit 2 (Address 0x03 -> data[5])
+    LDA #0x03
+    STA byte1
+    LDA #0x0F
+    STA byte2
+    
+    JSR bytes_send
+    
+    ; display 0 on digit 1 (Address 0x02 -> data[6])
+    LDA #0x02
+    STA byte1
+    LDA #0x0F
+    STA byte2
+    
+    JSR bytes_send
+    
+    ; display 1 on digit 0 (Address 0x01 -> data[7])
+    LDA #0x01
+    STA byte1
+    LDA #0x01
+    STA byte2
+    
+    JSR bytes_send
 
-    LDA #0x01 ; address for digit 0
-    STA output
-    JSR byte_send
-    LDA #0x01 ; data byte for number 0
-    STA output
-    JSR byte_send
 
     BRK ; end program
 
 
 
-byte_send: ; subroutine to send 8 bits (bit 7 is data, bit 6 is CS/SS, bit 5 is CLK)
+byte_send: ; subroutine to send 8 bits
 
     set_clk_low: ; set the clock low before getting data in pin 7
         LDA outputKIM
@@ -106,6 +131,35 @@ byte_send: ; subroutine to send 8 bits (bit 7 is data, bit 6 is CS/SS, bit 5 is 
         
     RTS ; end subroutine
 
+
+bytes_send: ; subroutine to send 2 bytes for MAX7219 display
+    
+    ; set SS pin low
+    set_ss_low:
+        LDA outputKIM
+        AND #0b10111111 ; Pull SS (bit 6) low
+        STA outputKIM
+    
+    ; send address byte
+    send_address:
+        LDA byte1
+        STA output
+        JSR byte_send
+    
+    ; send data byte
+    send_data:
+        LDA byte2
+        STA output
+        JSR byte_send
+    
+    ; set SS pin high
+    set_ss_high:
+        LDA outputKIM
+        ORA #0b01000000 ; Pull SS (bit 6) high
+        STA outputKIM
+        
+    RTS ; end subroutine
+    
 setup: ; setup subroutine
 
     clear_decimal_mode:
@@ -119,7 +173,7 @@ setup: ; setup subroutine
         LDA #0xFF
         STA outputSettings
     
-    set_low: ; Pull SS and CLK pin low by ANDing with outputKIM and storing it back
+    set_low: ; Pull SS and CLK pin to low by AND with outputKIM and storing it back
             LDA outputKIM
             AND #0b11011111  ; Pull CLK (bit 5) low
             AND #0b10111111  ; Pull SS (bit 6) low
@@ -127,8 +181,46 @@ setup: ; setup subroutine
             
     RTS
 
+initialize: ; Initialize MAX7219 subroutine
 
-.org 0x2000 ; 
+    ; Set Decode Mode to code B (Address 0x09 -> Data 0xFF)
+    LDA #0x09
+    STA byte1
+    LDA #0xFF
+    STA byte2
+    JSR bytes_send
+
+    ; Set Intensity (Address 0x0A -> Data 0x0F)
+    LDA #0x0A
+    STA byte1
+    LDA #0x0F
+    STA byte2
+    JSR bytes_send
+
+    ; Set Scan Limit (Address 0x0B -> Data 0x07)
+    LDA #0x0B
+    STA byte1
+    LDA #0x07
+    STA byte2
+    JSR bytes_send
+
+    ; Turn on display (Address 0x0C -> Data 0x01)
+    LDA #0x0C
+    STA byte1
+    LDA #0x01
+    STA byte2
+    JSR bytes_send
+
+    ; Exit Display Test Mode (Address 0x0F -> Data 0x00)
+    LDA #0x0F
+    STA byte1
+    LDA #0x00
+    STA byte2
+    JSR bytes_send
+    
+    RTS ; end subroutine
+
+
 
 ; put data or whatever here
 ; .include "file.txt" or something
